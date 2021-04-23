b450d4ae349920c1b9b904f8aa82ee14c0d37c74
Rafael Couto <rafael6291@gmail.com>
2021-04-23 16:16:39 -0300

    Making changes so the user can interact with app screens

--- lib/challenge/question_indicator/question_indicator_widget.dart
+++ lib/challenge/question_indicator/question_indicator_widget.dart
@@ -3,7 +3,10 @@ import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator.dar
 import 'package:flutter/material.dart';
 
 class QuestionIndicatiorWidget extends StatelessWidget {
-  const QuestionIndicatiorWidget({Key? key}) : super(key: key);
+  final int currentPage;
+  final int length;
+
+  const QuestionIndicatiorWidget({Key? key, required this.currentPage, required this.length}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +18,17 @@ class QuestionIndicatiorWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                "Questão 04",
+                "Questão $currentPage",
                 style: AppTextStyles.body,
               ),
               Text(
-                "de 10",
+                "de $length",
                 style: AppTextStyles.body,
               ),
             ],
           ),
           SizedBox(height: 16),
-          ProgressIndicatorWidget(value: 0.7),
+          ProgressIndicatorWidget(value: currentPage / length),
         ],
       ),
     );
